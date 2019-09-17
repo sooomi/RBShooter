4ba4177a66f0f38c67089a0483da86ed2c469e3f
sooomi <nur.almarsoomi@gmail.com>
2019-09-17 13:28:16 +0200

    Added base class for projectile

---
+++ Source/RBShooter/Combat/ProjectileBase.cpp
@@ -0,0 +1,27 @@
+// Copyright (c) 2019 PSQ Grupp 4 =)
+
+
+#include "ProjectileBase.h"
+
+// Sets default values
+AProjectileBase::AProjectileBase()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AProjectileBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AProjectileBase::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
