e807ef5f8cced38441780e9ef3c81eb79337316e
Jonatan <jonahenr@playgroundsquad.com>
2019-09-19 15:58:14 +0200

    Merge

---
+++ Source/RBShooter/Enemy/Enemy.cpp
@@ -0,0 +1,36 @@
+// Copyright (c) 2019 PSQ Grupp 4 =)
+
+
+#include "Enemy.h"
+#include "Gameframework/CharacterMovementComponent.h"
+
+// Sets default values
+AEnemy::AEnemy()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AEnemy::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	CachedMovementComponent = Cast<UCharacterMovementComponent>(GetMovementComponent());
+}
+
+// Called every frame
+void AEnemy::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AEnemy::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
