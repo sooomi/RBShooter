4bb2fd7044ec3eeb603e41586fd5ce801edaf0af
soomi <nur.almarsoomi@gmail.com>
2019-09-25 12:56:08 +0200

    Implemented enemy wave system

--- Source/RBShooter/RBShooterGameModeBase.cpp
+++ Source/RBShooter/RBShooterGameModeBase.cpp
@@ -3,11 +3,16 @@
 
 #include "RBShooterGameModeBase.h"
 #include "Level/EnemySpawnNode.h"
+#include "Kismet/GameplayStatics.h"
+#include "TimerManager.h"
+#include "Engine/World.h"
 
 ARBShooterGameModeBase::ARBShooterGameModeBase()
 {
 	PrimaryActorTick.bCanEverTick = false;
 
+	bWaveActive = false;
+	NextBurstNumEnemies = 0;
 	CurrentWave = 0;
 	CurrentWaveBurst = 0;
 }
@@ -17,9 +22,30 @@ ARBShooterGameModeBase::~ARBShooterGameModeBase()
 
 }
 
+void ARBShooterGameModeBase::BeginPlay()
+{
+	Super::BeginPlay();
+
+	ResetToDefault();
+}
+
 void ARBShooterGameModeBase::Tick(float DeltaSeconds)
 {
+	Super::Tick(DeltaSeconds);
+
 	// TODO enable tick in constructor before using this
+}
+
+void ARBShooterGameModeBase::ResetToDefault()
+{
+	StopWave();
+
+	bWaveActive = false;
+	NextBurstNumEnemies = 0;
+	CurrentWave = 0;
+	CurrentWaveBurst = 0;
+
+	UGameplayStatics::GetAllActorsOfClass(GetWorld(), AEnemySpawnNode::StaticClass(), EnemySpawnNodes);
 }
 
 EEnemySpawnTypes ARBShooterGameModeBase::GetRandomEnemySpawnType()
@@ -27,9 +53,103 @@ EEnemySpawnTypes ARBShooterGameModeBase::GetRandomEnemySpawnType()
 	return (EEnemySpawnTypes)FMath::RandRange(1, (int32)EEnemySpawnTypes::EST_COUNT - 1);
 }
 
-TArray<AActor*>& ARBShooterGameModeBase::GetRandomEnemySpawnNodes(int32 NumNodes)
+bool ARBShooterGameModeBase::StartWave(float WaveDuration, float BurstInterval, int32 FirstBurstNumEnemies)
 {
-	SelectedEnemySpawnNodes.Reset();
+	if (!bWaveActive)
+	{
+		bWaveActive = true;
+
+		CurrentWave++;
+		CurrentWaveBurst = 0;
+
+		// Start wave timer
+		GetWorldTimerManager().SetTimer(WaveTimerHandle, this, &ARBShooterGameModeBase::WaveTimerUpdate, WaveDuration, false, -1.0f);
+
+		StartBurst(BurstInterval, FirstBurstNumEnemies);
+
+		return true;
+	}
+
+	return false;
+}
+
+bool ARBShooterGameModeBase::StopWave()
+{
+	if (bWaveActive)
+	{
+		bWaveActive = false;
+
+		// Stop wave timer
+		GetWorldTimerManager().ClearTimer(WaveTimerHandle);
+
+		// Stop burst timer
+		StopBurst();
+
+		float TimeElapsed = GetWorldTimerManager().GetTimerElapsed(WaveTimerHandle);
+		OnWaveCompleted(CurrentWave, TimeElapsed);
+
+		return true;
+	}
+
+	return false;
+}
+
+bool ARBShooterGameModeBase::StartBurst(float BurstInterval, int32 NumEnemiesToSpawn)
+{
+	if (bWaveActive)
+	{
+		// Start burst timer
+		GetWorldTimerManager().SetTimer(BurstTimerHandle, this, &ARBShooterGameModeBase::BurstTimerUpdate, BurstInterval, false, -1.0f);
+
+		NextBurstNumEnemies = NumEnemiesToSpawn;
+
+		return true;
+	}
+
+	return false;
+}
+
+bool ARBShooterGameModeBase::StopBurst()
+{
+	// Stop burst timer
+	GetWorldTimerManager().ClearTimer(BurstTimerHandle);
+
+	return true;
+}
+
+bool ARBShooterGameModeBase::DoWaveBurst(int32 NumEnemiesToSpawn)
+{
+	TArray<AActor*> SelectedEnemySpawnNodes;
+	GetRandomEnemySpawnNodes(NumEnemiesToSpawn, SelectedEnemySpawnNodes);
+
+	if (SelectedEnemySpawnNodes.Num() > 0)
+	{
+		for (int32 i = 0; i < SelectedEnemySpawnNodes.Num(); i++)
+		{
+			AEnemySpawnNode* SpawnNode = Cast<AEnemySpawnNode>(SelectedEnemySpawnNodes[i]);
+			if (SpawnNode)
+			{
+				SpawnNode->ActivateSpawn();
+			}
+			else
+			{
+				UE_LOG(LogTemp, Error, TEXT("Spawn Node is not AEnemySpawnNode!"));
+			}
+		}
+
+		CurrentWaveBurst++;
+
+		OnWaveBurst(CurrentWave, CurrentWaveBurst, NumEnemiesToSpawn);
+
+		return true;
+	}
+
+	return false;
+}
+
+void ARBShooterGameModeBase::GetRandomEnemySpawnNodes(int32 NumNodes, TArray<AActor*>& OutNodes)
+{
+	OutNodes.Reset();
 
 	// Cant randomize more nodes than existing
 	NumNodes = FMath::Clamp(NumNodes, 0, EnemySpawnNodes.Num());
@@ -37,8 +157,8 @@ TArray<AActor*>& ARBShooterGameModeBase::GetRandomEnemySpawnNodes(int32 NumNodes
 	// Early exit - number of nodes to be randomized is equal to number of existing nodes (no randomization needed)
 	if (NumNodes == EnemySpawnNodes.Num())
 	{
-		SelectedEnemySpawnNodes = EnemySpawnNodes;
-		return SelectedEnemySpawnNodes;
+		OutNodes = EnemySpawnNodes;
+		return;
 	}
 
 	// Randomize nodes
@@ -54,9 +174,9 @@ TArray<AActor*>& ARBShooterGameModeBase::GetRandomEnemySpawnNodes(int32 NumNodes
 			RandomIndex = FMath::RandRange(0, EnemySpawnNodes.Num() - 1);
 
 			bool bFoundSameIndex = false;
-			for (int32 j = 0; j < SelectedEnemySpawnNodes.Num(); j++)
+			for (int32 j = 0; j < OutNodes.Num(); j++)
 			{
-				if (EnemySpawnNodes[RandomIndex] == SelectedEnemySpawnNodes[j])
+				if (EnemySpawnNodes[RandomIndex] == OutNodes[j])
 				{
 					bFoundSameIndex = true;
 				}
@@ -85,13 +205,25 @@ TArray<AActor*>& ARBShooterGameModeBase::GetRandomEnemySpawnNodes(int32 NumNodes
 		}
 
 		// "Select" the node by adding it to the array
-		SelectedEnemySpawnNodes.Add(EnemySpawnNodes[RandomIndex]);
+		OutNodes.Add(EnemySpawnNodes[RandomIndex]);
 	}
-
-	return SelectedEnemySpawnNodes;
 }
 
 bool ARBShooterGameModeBase::CanEnemyNodeBeSpawned(AEnemySpawnNode* SpawnNode)
 {
 	return true;
+}
+
+void ARBShooterGameModeBase::WaveTimerUpdate()
+{
+	StopWave();
+
+	UE_LOG(LogTemp, Warning, TEXT("WaveTimerUpdate"));
+}
+
+void ARBShooterGameModeBase::BurstTimerUpdate()
+{
+	DoWaveBurst(NextBurstNumEnemies);
+
+	UE_LOG(LogTemp, Warning, TEXT("BurstTimerUpdate"));
 }
--- Source/RBShooter/RBShooterGameModeBase.h
+++ Source/RBShooter/RBShooterGameModeBase.h
@@ -21,30 +21,52 @@ public:
 	ARBShooterGameModeBase();
 	~ARBShooterGameModeBase();
 
+	virtual void BeginPlay() override;
 	virtual void Tick(float DeltaSeconds) override;
 
+	/* Get random type from enum */
 	UFUNCTION(BlueprintCallable, Category="Enemy Spawning")
 	EEnemySpawnTypes GetRandomEnemySpawnType();
 
+	/* Get randomly selected nodes from available nodes in the level */
 	UFUNCTION(BlueprintCallable, Category = "Enemy Spawning")
-	TArray<AActor*>& GetRandomEnemySpawnNodes(int32 NumNodes);
+	void GetRandomEnemySpawnNodes(UPARAM(DisplayName="Number of Nodes") int32 NumNodes, TArray<AActor*>& OutNodes);
 
-	UFUNCTION(BlueprintCallable, BlueprintImplementableEvent, Category="Wave Management")
-	bool DoWaveBurst(int32 NumberOfEnemiesToSpawn);
+	/* Start a wave if not currently in progress. @return True if wave was started */
+	UFUNCTION(BlueprintCallable, Category="Wave Management")
+	bool StartWave(float WaveDuration = 30.0f, float BurstInterval = 10.0f, UPARAM(DisplayName = "Number Of Enemies To Spawn On First Burst") int32 FirstBurstNumEnemies = 5);
 
+	/* Stops a wave and its timers. @return True if wave was stopped */
+	UFUNCTION(BlueprintCallable, Category="Wave Management")
+	bool StopWave();
+
+	/* Starts a burst timer if wave is in progress. */
+	UFUNCTION(BlueprintCallable, Category="Wave Management")
+	bool StartBurst(float BurstInterval, UPARAM(DisplayName = "Number of Enemies to Spawn") int32 NumEnemiesToSpawn);
+
+	/* Stops a burst timer. @return True if burst was stopped */
+	UFUNCTION(BlueprintCallable, Category = "Wave Management")
+	bool StopBurst();
+
+	/* Spawn enemies from available nodes in the level */
+	UFUNCTION(BlueprintCallable, Category="Wave Management")
+	bool DoWaveBurst(UPARAM(DisplayName = "Number of Enemies to Spawn") int32 NumEnemiesToSpawn);
+
+	/* When a wave is completed */
 	UFUNCTION(BlueprintImplementableEvent, Category = "Wave Management")
 	void OnWaveCompleted(int32 WaveNumber, float TimeLeft);
 
+	/* When a burst is completed. A wave can consist of several bursts. */
 	UFUNCTION(BlueprintImplementableEvent, Category = "Wave Management")
-	void OnWaveBurst(int32 WaveNumber, int32 BurstNumber);
+	void OnWaveBurst(int32 WaveNumber, int32 BurstNumber, int32 NumEnemiesSpawned);
+
+	UFUNCTION(BlueprintCallable, Category="Wave Management")
+	void ResetToDefault();
 
 public:
 
 	UPROPERTY(BlueprintReadWrite, Category = "Enemy Spawning")
 	TArray<AActor*> EnemySpawnNodes;
-
-	UPROPERTY(BlueprintReadOnly, Category = "Enemy Spawning")
-	TArray<AActor*> SelectedEnemySpawnNodes;
 
 	UPROPERTY(BlueprintReadOnly, Category="Wave Management")
 	FTimerHandle WaveTimerHandle;
@@ -52,13 +74,26 @@ public:
 	UPROPERTY(BlueprintReadOnly, Category = "Wave Management")
 	FTimerHandle BurstTimerHandle;
 
+	UPROPERTY(BlueprintReadWrite, Category="Wave Management")
+	float WaveInterval;
+
 	UPROPERTY(BlueprintReadOnly, Category = "Wave Management")
 	int32 CurrentWave;
 
 	UPROPERTY(BlueprintReadOnly, Category = "Wave Management")
 	int32 CurrentWaveBurst;
 
+	UPROPERTY(BlueprintReadOnly, Category="Wave Management")
+	bool bWaveActive;
+
 private:
 
 	bool CanEnemyNodeBeSpawned(AEnemySpawnNode* SpawnNode);
+
+	void WaveTimerUpdate();
+	void BurstTimerUpdate();
+
+private:
+
+	int32 NextBurstNumEnemies;
 };
